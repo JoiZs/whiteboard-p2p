2789d88c8fac0ed203f82964062df2b6cfad0d49
Jay <janghyeongjae@Jays-MacBook-Pro-2.local>
2024-08-02 11:35:00 -0400

    joined room function created

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,18 +1,28 @@
 import createRoom from "../utils/generateRoomId";
-import socket from "../utils/socket";
+import JoinRoomFn from "../utils/joinRoom";
+import { HtmlHTMLAttributes, useRef } from "react";
 
 type Props = {};
 
 const Home = (props: Props) => {
+  const roomId = useRef<HTMLInputElement>(null);
+  const statusHandler = async () => {
+    const status = await JoinRoomFn(roomId.current!.value);
+    if (status.status === "room-joined") {
+      console.log("Room Joined");
+    } else {
+      console.log("Room not found");
+    }
+  };
   return (
     <div className="w-full h-full flex justify-center items-center">
       <div className="flex flex-col gap-2">
         <div>
-          <input type="text" placeholder="Room Id" />
-          <button>Join</button>
+          <input type="text" placeholder="Room Id" ref={roomId} />
+          <button onClick={statusHandler}>Join</button>
         </div>
         <button className="w-full bg-red-500" onClick={createRoom}>
-          Create{" "}
+          Create
         </button>
       </div>
     </div>
