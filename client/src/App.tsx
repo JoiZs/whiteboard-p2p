e5b89b2d3dd401b8c662dfb108c37a2de01a9bf9
Jay <janghyeongjae@Jays-MacBook-Pro-2.local>
2024-08-12 10:24:52 -0400

    home page styled

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,16 @@
 import Home from "./pages/home";
+import Room from "./pages/room";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="w-full h-screen">
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/room" element={<Room />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,30 +1,44 @@
 import createRoom from "../utils/generateRoomId";
 import JoinRoomFn from "../utils/joinRoom";
-import { HtmlHTMLAttributes, useRef } from "react";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {};
 
 const Home = (props: Props) => {
+  const navigate = useNavigate();
   const roomId = useRef<HTMLInputElement>(null);
   const statusHandler = async () => {
     const status = await JoinRoomFn(roomId.current!.value);
     if (status.status === "room-joined") {
       console.log("Room Joined");
+      navigate("/room");
     } else {
       console.log("Room not found");
     }
   };
   return (
-    <div className="w-full h-full flex justify-center items-center">
-      <div className="flex flex-col gap-2">
-        <div>
-          <input type="text" placeholder="Room Id" ref={roomId} />
-          <button onClick={statusHandler}>Join</button>
+    <div className="w-full h-full flex flex-col justify-center items-center bg-teal-50 gap-4">
+      <div className="text-teal-500 text-5xl mb-4 font-logo">Marker</div>
+      <div className="flex flex-col bg-teal-50 w-1/4 justify-center items-center border-2 border-teal-100 rounded shadow-md">
+        <div className="flex flex-row justify-center py-2 w-full">
+          <input
+            type="text"
+            placeholder="Room Id"
+            ref={roomId}
+            className="mr-5 outline-teal-100 p-1 text-teal-400 w-4/5 bg-teal-50"
+          />
+          <button onClick={statusHandler} className="text-teal-400">
+            Join
+          </button>
         </div>
-        <button className="w-full bg-red-500" onClick={createRoom}>
-          Create
-        </button>
       </div>
+      <button
+        className="w-1/4 bg-teal-400 p-1 mb-3 text-white rounded"
+        onClick={createRoom}
+      >
+        Create
+      </button>
     </div>
   );
 };
